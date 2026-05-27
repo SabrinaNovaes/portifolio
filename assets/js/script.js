c3b0134c1470ff6a0a8b627f53893ad68c580ded
= <=>
2026-05-26 23:34:20 -0300

    🔨 Refactor Code:

--- assets/js/script.js
+++ assets/js/script.js
@@ -378,6 +378,32 @@ formulario.addEventListener("submit", function (event) {
     }
 })
 
+const cursor = document.querySelector('.magic-cursor');
+
+document.addEventListener('mousemove', (e) => {
+
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+
+    createSpark(e.clientX, e.clientY);
+});
+
+function createSpark(x, y) {
+
+    const spark = document.createElement('div');
+
+    spark.classList.add('spark');
+
+    spark.style.left = x + 'px';
+    spark.style.top = y + 'px';
+
+    document.body.appendChild(spark);
+
+    setTimeout(() => {
+        spark.remove();
+    }, 800);
+}
+
 // Executar a função getAboutGitHub
 getAboutHitHub();
 
